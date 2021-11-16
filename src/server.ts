7c10f197fec45f799ff3711f351c829e2924ed8d
brunoandrade <bhmandrade411@gmail.com>
2021-11-15 22:14:42 -0300

    First commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { routes } from "./routes";
+import cors from "cors";
 
 const PORT = process.env.PORT || 10110;
 const app = express();
 
+const corsConfig = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use('/api', routes);
 
